fdd9a506158b20cbb5351985811866e6bc24897a
manuel <manuelmolinuevo22@gmail.com>
2020-06-10 03:57:01 -0300

    RoundDuration

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
-let playButton = document.querySelector(".fa-play");
+//dom elements
+let playButton = document.querySelector("#play");
 let timer = document.querySelector(".time");
 let backwardButton = document.querySelector(".fa-backward");
-let stopCountdown = false;
+let forwardButton= document.querySelector('.fa-forward');
+let circle = document.querySelector(".circle");
+let sess = document.querySelector("#session");
+let roundNumber = document.querySelector("#rounds");
 
-function countdown(){
-    playButton.removeEventListener('click', countdown);
-    let seconds;
-    let interval = setInterval(function(){
-        if(stopCountdown === true){
-            clearInterval(interval);
-            playButton.addEventListener('click', countdown);
-            stopCountdown = false;
-            return;
-        }
-        seconds = minutesToSeconds();
-        seconds--;
-        timer.textContent = secondsToMinutes(seconds);
-        if(seconds <= 0){
-            clearInterval(interval);
-        }
-    }, 1000);
+
+let type = 'Work';
+let isClockRunning = false;
+let workRoundDuration = 25*60;
+let breakRoundDuration = 5*60;
+let timeLeft = workRoundDuration;
+let numberOfBreaks = 0;
+let roundsPerSession = 4;
+let numberOfSessions = 2;
+let totalRounds = roundsPerSession*numberOfSessions;
+
+const toggleClock = (reset) => {
+    if(reset){
+       clearInterval(interval);
+       timeLeft = workRoundDuration
+       timer.textContent = displayTime();
+       if(isClockRunning === true){
+           isClockRunning = false;
+           togglePlayButton();
+       }
+    } else {
+    if (isClockRunning === true){
+       clearInterval(interval);
+       isClockRunning = false;
+    } else {
+        isClockRunning = true;
+        interval = setInterval(() => {
+            stepDown();
+            timer.textContent = displayTime();
+        }, 1000);
+    }
+  }
 }
 
-// setTimer(25)
-// startTimer()
-// pauseTimer()
-
-function minutesToSeconds () {
-    let arr = timer.textContent.split(":");
-    return parseInt(arr[0])*60 + parseInt(arr[1]);
-}
-
-function secondsToMinutes(seconds){
-    let m = Math.floor(seconds / 60);
-    let s = seconds%60;
+function displayTime(){
+    let m = Math.floor(timeLeft / 60);
+    let s = timeLeft%60;
     if(m<10){
         m = `0${m}`;
     }
@@ -43,13 +53,52 @@ function secondsToMinutes(seconds){
     return `${m}:${s}`;
 }
 
-function skipBack(s){
-    stopCountdown = true;
-    timer.textContent =  `${s}:00`;
+function togglePlayButton(){
+    playButton.classList.toggle("fa-play");
+    playButton.classList.toggle("fa-pause");
+}
+
+function stepDown() {
+    if(timeLeft === 0){
+        if(type === "Work"){
+            type = "Break";
+            circle.style.borderColor = "#56f25a";
+            sess.textContent = "Break";
+            numberOfBreaks++;
+            if(numberOfBreaks === 4){
+                timeLeft = breakRoundDuration*2;
+            }else{
+                timeLeft = breakRoundDuration;
+            }
+        }else{
+            type ="Work";
+            timeLeft = workRoundDuration;
+            if(numberOfBreaks === 4){
+                clearInterval(interval);
+                togglePlayButton();
+                isClockRunning = false;
+                numberOfBreaks = 0;
+            }
+            circle.style.borderColor= "#e7363e";
+            sess.textContent = "Working";
+            roundNumber.textContent = `${numberOfBreaks+1}/${roundsPerSession}`;
+        }
+    }else{
+        timeLeft--;
+    }
 }
 
 
+//event listeners
+playButton.addEventListener("click", () => {
+    toggleClock();
+    togglePlayButton();
+});
 
-playButton.addEventListener("click", countdown);
-backwardButton.addEventListener("click", () => skipBack(25));
+backwardButton.addEventListener("click", () => {
+    toggleClock(true);
+});
 
+forwardButton.addEventListener("click", () => {
+    timeLeft = 0;
+});
