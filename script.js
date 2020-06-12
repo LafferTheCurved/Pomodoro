5730892b336b5fa9bc9d0aae3084a60f27073383
manuel <manuelmolinuevo22@gmail.com>
2020-06-12 03:49:54 -0300

    Settings

--- script.js
+++ script.js
@@ -8,8 +8,15 @@ let sess = document.querySelector("#session");
 let roundNumber = document.querySelector("#rounds");
 let bars = document.querySelector('#bars');
 let settingsPage = document.querySelector('.settings-page');
-settingsPage.style.display = "none";
+let pointsRounds = document.querySelector("#points-rounds");
+let pointsWork = document.querySelector('#points-work');
+let pointsBreak = document.querySelector('#points-break');
+let outputWork = document.querySelector('#output-work');
+let outputBreak = document.querySelector('#output-break');
+let outputRounds = document.querySelector('#output-rounds');
+let resetSession = document.querySelector('#reset-session');
 
+settingsPage.style.display = "none";
 let type = 'Work';
 let isClockRunning = false;
 let workRoundDuration = 25*60;
@@ -17,13 +24,15 @@ let breakRoundDuration = 5*60;
 let timeLeft = workRoundDuration;
 let numberOfBreaks = 0;
 let roundsPerSession = 4;
-let numberOfSessions = 2;
-let totalRounds = roundsPerSession*numberOfSessions;
 
 const toggleClock = (reset) => {
     if(reset){
        clearInterval(interval);
-       timeLeft = workRoundDuration
+       if(type === "Break"){
+           timeLeft = breakRoundDuration;
+       }else{
+           timeLeft = workRoundDuration;
+       }
        timer.textContent = displayTime();
        if(isClockRunning === true){
            isClockRunning = false;
@@ -80,7 +89,7 @@ function stepDown() {
             circle.style.borderColor = "#56f25a";
             sess.textContent = "Break";
             numberOfBreaks++;
-            if(numberOfBreaks === 4){
+            if(numberOfBreaks === roundsPerSession){
                 timeLeft = breakRoundDuration*2;
             }else{
                 timeLeft = breakRoundDuration;
@@ -88,7 +97,7 @@ function stepDown() {
         }else{
             type ="Work";
             timeLeft = workRoundDuration;
-            if(numberOfBreaks === 4){
+            if(numberOfBreaks === roundsPerSession){
                 clearInterval(interval);
                 togglePlayButton();
                 isClockRunning = false;
@@ -121,4 +130,28 @@ forwardButton.addEventListener("click", () => {
 bars.addEventListener("click", () => {
     toggleDisplay();
     toggleMenuButton();
-})
+});
+
+pointsWork.addEventListener("input", () => {
+    let p = pointsWork.value;
+    if(p<10){
+        p = `0${p}`;
+    }
+    outputWork.textContent = `${p}:00`;
+});
+
+pointsRounds.addEventListener("input", () => {
+    outputRounds.textContent = pointsRounds.value;
+});
+
+pointsBreak.addEventListener("input", () => {
+    let p = pointsBreak.value;
+    if(p<10){
+        p = `0${p}`;
+    }
+    outputBreak.textContent = `${p}:00`;
+});
+
+resetSession.addEventListener("click", () => {
+    //reset
+});
